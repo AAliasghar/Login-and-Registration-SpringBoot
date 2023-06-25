5e9a431b7c1c6a1214b1564675e993f964c8f298
AAliasghar <a.asgharinia@yahoo.com>
2023-06-25 22:25:39 +0200

    DataBase

---
+++ login_registeration.sql
@@ -0,0 +1,17 @@
+-- -----------------------------------------------------
+-- Schema Login Register ------
+-- -----------------------------------------------------
+-- DROP SCHEMA IF EXISTS `login_register_db`;
+
+-- CREATE DATABASE `login_register_db`;
+ USE `login_register_db` ;
+
+-- -----------------------------------------------------
+-- TABLE DEPARTMENTS
+-- -----------------------------------------------------
+SELECT * FROM users ;
+SELECT * FROM roles ;
+SELECT * FROM users_roles;
+
+
+
